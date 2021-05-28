cb6380d105d6f3a4ddd83b9070359a5e08ad7860
LeeWoojin-99 <lwj1316@naver.com>
2021-05-29 05:12:51 +0900

    2021 05 29

--- English_Word_Test_Program.py
+++ English_Word_Test_Program.py
@@ -6,19 +6,20 @@ import time
 
 class WindowClass(QMainWindow, Ui_MainWindow):
 	def __init__(self):
+		global english_excel_class
 
 		# 초기 설정
 		super(WindowClass, self).__init__()
 		self.setupUi(self)
 
-		self.row = excel.start_row
+		self.rows = english_excel_class.rows
 		self.setWindowTitle("영어 공부를 하자")
 
 		self.answer_button.clicked.connect(self.answer_function)
 		# QPushButton 클래스 위젯이 클릭되면 해당 함수를 실행
 		self.next_problem.clicked.connect(self.next_problem_function)
 
-		self.problem.setPlainText(excel.cell_data_func(self.row, 1))
+		self.problem.setPlainText(english_excel_class.cell_data_func(self.rows[0], 1))
 		self.problem.setAlignment(QtCore.Qt.AlignCenter)
 		# 가로 가운데 정렬
 		#self.problem.setVerticalAlignment(QtCore.Qt.AlignCenter)
@@ -28,37 +29,45 @@ class WindowClass(QMainWindow, Ui_MainWindow):
 		self.answer_input.returnPressed.connect(self.answer_function)
 		# QLineEdit 클래스 위젯에서 Enter키가 눌릴 때마다 해당 함수를 실행
 
-
-
 	def answer_function(self):
+		# 시험지에 적혀있는 것이 정답인지 채점하는 함수
 
-		if self.row > excel.end_row:
+		if len(self.rows) == 0:
+			# 남아있는 문제가 없다면
+			self.answer_input.clear()
 			return 0
 
-		if self.answer_input.text() == excel.cell_data_func(self.row, 2):
+		if self.answer_input.text() == english_excel_class.cell_data_func(self.rows[0], 2):
+			# 정답인 경우
+
 			print("정답")
 			#self.result.setPlainText("정답입니다.\n2초 후에 다음문제로 넘어갑니다.")
 			#time.sleep(2)
-			self.result.setPlainText("정답\n" + excel.cell_data_func(self.row, 1) + "\n" + excel.cell_data_func(self.row, 2))
+			self.result.setPlainText("정답\n" + english_excel_class.cell_data_func(self.rows[0], 1) + "\n" + english_excel_class.cell_data_func(self.rows[0], 2))
 			# 정답이라면 초기 상태로 돌아가기
 
-			self.row += 1
-			# 다음 문제로 넘어가기
-			self.problem.setPlainText(excel.cell_data_func(self.row, 1))
-			self.problem.setAlignment(QtCore.Qt.AlignCenter)
-			# 다음 문제 출력
+			# 다음 문제로 넘어가는 과정
+			del self.rows[0]
+			# 맞춘 문제 지우기
 
-			if self.row == excel.end_row:
+			# 남아있는 문제가 있는지 판별하는 과정
+			if len(self.rows) > 0:
 				# 마지막 문제
 				print("마지막 문제입니다.")
-			elif self.row > excel.end_row:
-				# 시험 범위 초과
-				self.result.setPlainText("문제를 모두 완료하였습니다.")
-				self.result.setAlignment(QtCore.Qt.AlignCenter)
+			else:
+				# 남아있는 문제가 없는 경우
+				self.result.append("문제를 모두 완료하였습니다.")
+				#self.result.setAlignment(QtCore.Qt.AlignCenter)
 				# 결과 창에 출력
 				self.problem.setPlainText("문제를 모두 완료하였습니다.")
 				self.problem.setAlignment(QtCore.Qt.AlignCenter)
 				# 문제 창에 출력
+				self.answer_input.clear()
+				return 0
+
+			self.problem.setPlainText(english_excel_class.cell_data_func(self.rows[0], 1))
+			self.problem.setAlignment(QtCore.Qt.AlignCenter)
+			# 다음 문제 출력
 
 			self.answer_input.clear()
 			# 입력 창을 비우기
@@ -72,15 +81,31 @@ class WindowClass(QMainWindow, Ui_MainWindow):
 			# 입력 창을 비우기
 
 	def next_problem_function(self):
-		self.row += 1
-
-		if self.row > excel.end_row:
+		if len(self.rows) == 0:
+			# 버튼을 눌렀는데 남아있는 문제가 없다면
+			self.answer_input.clear()
 			return 0
 
-		self.problem.setPlainText(excel.cell_data_func(self.row, 1))
+		self.result.setPlainText("다음 문제로 넘어왔습니다.")
+		self.result.append("정답\n" + english_excel_class.cell_data_func(self.rows[0], 1) + "\n" + english_excel_class.cell_data_func(self.rows[0], 2))
+
+		del self.rows[0]
+		# 넘어간 문제 지우기
+
+		if len(self.rows) == 0:
+			# 넘어갔는데 남아있는 문제가 없다면
+			self.result.setPlainText("남아있는 문제가 없습니다.")
+			self.result.setAlignment(QtCore.Qt.AlignCenter)
+			# 결과 창에 출력
+			self.problem.setPlainText("남아있는 문제가 없습니다.")
+			self.problem.setAlignment(QtCore.Qt.AlignCenter)
+			# 문제 창에 출력
+			self.answer_input.clear()
+			return 0
+
+		self.problem.setPlainText(english_excel_class.cell_data_func(self.rows[0], 1))
 		self.problem.setAlignment(QtCore.Qt.AlignCenter)
 		# 다음 문제 출력
-		self.result.setPlainText("다음 문제로 넘어왔습니다.")
 
 
 '''
@@ -89,8 +114,13 @@ answer_input : QTextEdit
 problem_label : QTextBrowser
 '''
 
+# 초기 설정값 3개
+day = 1
+start = 2
+end = 5
+
 app = QApplication()
-excel.end_row = 15
+english_excel_class = excel.EnglishExcel(day, start, end)
 window = WindowClass()
 window.show()
 app.exec()
--- english_excel_file.py
+++ english_excel_file.py
@@ -1,39 +1,38 @@
 import openpyxl as xl
 import random
 
-def cell_data_func(func_row, func_column):
-    # row, column 값을 입력받아 해당 위치의 데이터를 반환해주는 함수
-    return sheet[chr(64+func_column)+str(func_row)].value
+class EnglishExcel:
+    def __init__(self, day, start_row, end_row):
+        self.workbook = xl.load_workbook('E:\영단어.xlsx')
 
-def find_merged_cell(func_row):
-    # 병합된 셀의 뜻을 찾아주는 함수
-    global rows
-    func_row += 1
+        self.day = "Day " + str(day)
+        self.sheet = self.workbook[self.day]
 
-    if cell_data_func(func_row, 1) == None:
-        print("뜻 : ", cell_data_func(func_row, 2), sep='')
-        del rows[rows.index(func_row)]
-        find_merged_cell(func_row)
+        self.start_row = 2
+        self.end_row = self.sheet.max_row
 
-workbook = xl.load_workbook('E:\영단어.xlsx')
+        self.rows = list(range(start_row, end_row+1))
+        random.shuffle(self.rows)
 
-#day = input("Day를 입력해주세요 : ")
-#start_row, end_row = map(int, input("시작 행과 끝 행을 적어주세요 : "))
-day = "11"
-day = "Day "+day
+        ''' TEST Code
+        for self.row in self.rows:
+            print("영단어 : ", self.cell_data_func(self.row, 1), sep='')
+            print("뜻 : ", self.cell_data_func(self.row, 2), sep='')
+            self.find_merged_cell(self.row)
+        '''
 
-sheet = workbook[day]
+    def cell_data_func(self, func_row, func_column):
+        # row, column 값을 입력받아 해당 위치의 데이터를 반환해주는 함수
+        return self.sheet[chr(64+func_column)+str(func_row)].value
 
-start_row = 2
-end_row = sheet.max_row
-end_row = 3
+    def find_merged_cell(self, func_row):
+        # 병합된 셀의 뜻을 찾아주는 함수
+        global rows
+        func_row += 1
 
-rows = list(range(start_row, end_row+1))
-#random.shuffle(rows)
+        if self.cell_data_func(func_row, 1) == None:
+            print("뜻 : ", self.cell_data_func(func_row, 2), sep='')
+            del self.rows[self.rows.index(func_row)]
+            self.find_merged_cell(func_row)
 
-'''
-for row in rows:
-    print("영단어 : ", cell_data_func(row, 1), sep='')
-    print("뜻 : ", cell_data_func(row, 2), sep='')
-    find_merged_cell(row)
-'''
+test_class = EnglishExcel(11, 2, 5)
